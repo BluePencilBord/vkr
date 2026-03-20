0532ec8e8a52928452accc4956d95e0ed8f15cd4
BluePencilBord <amin54321@yandex.ru>
2026-03-20 23:10:33 +0300

    initial database setup

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from app.api import users
+from app.config import settings
 
 
 app = FastAPI(
-    title = "GDD analysis API",
+    title = settings.project_name,
+    version = settings.version,
     description = "Бэк для мультиагентной системы анализа GDD"
 )
+app.include_router(users.router)
 
 @app.get("/")
 async def root():
